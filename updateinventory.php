f4400ff8dc1590f3494e83b499623c0d35042073
Yrassim <74272046+Yrassim@users.noreply.github.com>
2021-02-26 12:42:44 -0500

    Update updateinventory.php
    Working Update Products

--- updateinventory.php
+++ updateinventory.php
@@ -1,136 +1,154 @@
 <?php
 
-/* 
- * $_POST is an array that has key value pairs, for example if you have a textbox
- * with the name updateInv, you can use it to pass the values from the textbox
- */
-if(isset($_POST['updateInv'])){
-
-	// Since we are now working with Microsoft SQL server, which has a different PHP driver for connections
-	// we may need to download them onto server from here so the code executes properly: 
-	// https://docs.microsoft.com/en-us/sql/connect/php/microsoft-php-driver-for-sql-server?view=sql-server-ver15
-	
-	// $serverName is just the URL our database is hosted on:
 	$serverName = "database-1.cwszuet1aouw.us-east-1.rds.amazonaws.com";
-	// $connection info is an array which can only take database, user & password:
+	// $connection info to the database.
 	$connectionInfo = array( "Database"=>'yellowteam', "UID"=>'admin', "PWD"=>'$LUbx6*xTY957b6');
 	$conn = sqlsrv_connect( $serverName, $connectionInfo);
 
-	// Connects to server, or spits out error log if it fails
-	if( $conn ) {
-		 echo "Connection established.\n";
-	}else{
-		 echo "Connection could not be established.\n";
-		 die( print_r( sqlsrv_errors(), true));
+if(isset($_POST['submit'])){
+	
+	function function_alert($message) { 
+      
+		// Display the alert box  
+		echo "<script>alert('$message');</script>"; 
 	}
 	
-	//------------------------------------------- 
-	// itemID is (PK, int, not null)
-	// productName is (varchar(60), null)
-	// productSKU is (varchar(10), null)
-	// itemDescription is (varchar(5000), null)
-	// price is (smallmoney, null)
-	//-------------------------------------------
-	// Note: Update is NOT ALLOWED to change the productName according to the story requirements
-	
-	// placeholders (?) are used in SQL statements to prepare a statement & prevent SQL injection
-	$sql = "UPDATE dbo.inventory
-			SET productSKU = ?, itemDescription = ?, price = ?
-			WHERE itemID = ?";
-	// ** since productName & itemID are both unique, maybe productName is better to use for selecting search results from HTML
+	// Condition to check if there is no data given.
+	if ($_POST['product'] == "" or $_POST['update']=="" or $_POST['newUpdate']==""){
 		
-	//----------------------------------------------------------------------------------------------------------------  
-	// This is where we will need to add something in introducing pulling user input from the HTML to fill
-	// in the variables that are going into the $sql string. I think we just need to get some text fields created
-	// and name in the HTML, which we then feed in somehow (I'll look into this more later). 
-	//----------------------------------------------------------------------------------------------------------------  
-	// Note: going to have to create a search that spits out rows of results also? 
-	$productSKU = '';
-	$itemDescription = '';
-	$price = '';
-	$itemID = '';
+		// Function call 
+		function_alert("Missing Information. Please fulfill everything.");
 	
-	// prepares our statement with connection info, all variables inside placeholders in sql:
-	$stmt = sqlsrv_prepare( $conn, $sql, array( &$productSKU, &$itemDescription, &$price, &$itemID));
+	// Other conditions to update the database.	
+	}else{
+		$selected1 = $_POST['product'];
+		$selected2 = $_POST['update'];
+		
+		// Check wich part the user want to change, and use the SQL query to update it in the database.
+		
+		if($selected2 == "description"){
+			
+			$selected3 = $_POST['newUpdate'];		
+			sqlsrv_query( $conn, "UPDATE inventory SET itemDescription = '" .$selected3. "' WHERE productName='" .$selected1. "'");
+								
+		}elseif($selected2 == "price"){
+			
+			$selected3 = (float)$_POST['newUpdate'];
+			
+			sqlsrv_query( $conn, "UPDATE inventory SET price = '" .$selected3. "' WHERE productName ='" .$selected1. "'");
+						
+		}elseif($selected2 == "sku"){
+			
+			$selected3 = $_POST['newUpdate'];
+			sqlsrv_query( $conn, "UPDATE inventory SET productSKU = '" .$selected3. "' WHERE productName ='" .$selected1. "'");
+						
+		}else{
+			
+			$selected3 = "Please select one.";
+			
+		}
+		
+	 // Function call 
+	function_alert("Updated!  ". $selected1 . ", " . $selected2 . ", to: " . $selected3);
 	
-	// Checks that there's no issues with the statement, connection, parameters etc. once all pieced together:
-	if( $stmt )  
-	{  
-		 echo "Statement prepared.\n";  
-	}  
-	else  
-	{  
-		 echo "Error in preparing statement.\n";  
-		 die( print_r( sqlsrv_errors(), true));  
-	} 
-	
-	// This actually uses the statement on the database, prints out errors if something happens:
-	if( sqlsrv_execute( $stmt))  
-	{  
-		  echo "Statement executed.\n";  
-	}  
-	else  
-	{  
-		 echo "Error in executing statement.\n";  
-		 die( print_r( sqlsrv_errors(), true));  
-	}  
-	
-	// Closes the connection and also releases statement resources:
-	sqlsrv_free_stmt( $stmt);  
-	sqlsrv_close( $conn); 
-
+	}
 }
 ?>
 
-
 <!DOCTYPE html>
-<!--Yellow Team
-Jeremy Wellman
-Kyle Snyder
-Nicholas Seelbach
-Rassim Yahioune
-Thomas Rothwell
-Tomas Kasparaitis
-2/16/2021-->
-<html>
 
-<head>
-  <meta charset="utf-8">
-  <title>Update Products</title>
-  <meta name="author" content="Team Yellow">
-  <meta name="description" content="Nuts and bolts hardware company search for/update products page">
-  <meta name="keywords" content="Nuts and bolts, hardware, Nuts and bolts hardware, products, search, update, update products, search products">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
 
-  <link href="css/updateinventorystyle.css" rel="stylesheet">
-</head>
+    <head>
 
-<body>
-<div class="header">
+        <!-- documentation at http://getbootstrap.com/docs/4.1/, alternative themes at https://bootswatch.com/ -->
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
+
+        <link href="./css/styles.css" rel="stylesheet">
+        <link href="css/indexstyle.css" rel="stylesheet">
+        <title>Update Products</title>
+
+    </head>
+
+    <body>
+
+	<div class="header">
       <div class="links">
       <a class="active" href="index.html">Home</a>
-      <a href="contactus.html">Contact Us</a>
-      <a href="aboutus.html">FAQ</a>
 	  <a href="products.html">Products</a>
-      <a href="addinventory.php">Add Inventory</a>
+      <a href="addinventory.html">Add Inventory</a>
 	  <a href="updateinventory.php">Update Products</a>
+	  <a href="contactus.html">Contact Us</a>
+      <a href="aboutus.html">FAQ</a>
       </div>
       <div class ="logo">
       <img src="inverselogo.png" alt="Italian Trulli" style="width:4.5%;height:4.5%;">
     </div>
     </div>
-  
-  <h1>Nuts and Bolts Hardware</h1>
-  <h3>Welcome to the nuts and bolts search for or update products page!</h3>
-  
- 
- 
-  <!--No idea how to search for stuff in the database. -jeremy-->
-</form> 
+	<br><br>
 
-  
 
-  <script src="js/script.js"></script>
-</body>
+        <main class="container p-5">
+				
+			<div>
+				<b> Update Products <b>
+			</div>
+			<br>
+
+        <main class="container p-5">
+		
+
+
+			<form action="" method="POST">
+			  <select name='product'>
+			  
+				<option value="">Choose the product to update</option>
+			  
+				
+				<?php
+				// using php in the <select> to show dynamically the product in the webpage.
+				$sql = "SELECT * FROM inventory";
+
+				$query = sqlsrv_query( $conn, $sql);
+				if( $query === false ) {
+					 die( print_r( sqlsrv_errors(), true));
+				}
+				// A loop function to display all the products in the database.
+				while( $products = sqlsrv_fetch_array( $query, SQLSRV_FETCH_ASSOC) ) {
+				
+						$product=$products["productName"];?>
+						<option value="<?php echo $product; ?>"><?php
+							echo $product;?>
+						</option>
+					
+				<?php
+				}?>
+								
+			  </select>
+			  <br><br>
+			 
+
+			  <select name="update">
+				<option value="">Choose what to change from the product</option>
+				<option value="sku">Product SKU</option>
+				<option value="price">Price</option>
+				<option value="description">Description</option>
+			  </select>
+			  <br><br>
+			  <input type='text' name='newUpdate' id='newUpdate' placeholder="The New Update">
+			  <br><br>
+			  <button name="submit" class="btn btn-dark">Update</button>
+			</form>
+			<br></br>
+	
+        </main>
+
+        <footer>
+		<div>
+            Service provided by YellowTeam 2021
+		</div>
+        </footer>
+
+    </body>
 
 </html>
