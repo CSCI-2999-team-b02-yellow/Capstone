dbd012555233e3408b038152ccad5d89ed46bdfd
Tomas Kasparaitis <61473801+t-kasparaitis@users.noreply.github.com>
2021-02-27 05:28:32 -0500

    new file to not interfere with Rassim's version
    this is a work in progress, nothing works here, mostly introduced radiobuttons for the search and working on getting search results to display in table format
    afterward, will start work on selecting a result from the table to update specifically

---
+++ updateinventoryV2.php
@@ -0,0 +1,175 @@
+<?php
+
+	$serverName = "database-1.cwszuet1aouw.us-east-1.rds.amazonaws.com";
+	// $connection info to the database.
+	$connectionInfo = array( "Database"=>'yellowteam', "UID"=>'admin', "PWD"=>'$LUbx6*xTY957b6');
+	$conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+// Introducing separate search functionality, which would generate a table inside of the HTML
+if(isset($_POST['searchPress'])) {
+	$sql = "SELECT * FROM yellowteam.dbo.inventory WHERE ".$searchOption." = ?";
+	$searchOption = $_POST['searchOptions'];
+	$userInput = $_POST[''];
+	
+	// sanitizing $searchOption (vulnerable radiobutton value) which can only be 3 strict strings:
+	if ($searchOption == 'productSKU' OR
+		$searchOption == 'itemDescription' OR
+		$searchOption == 'price') 
+		{
+		// prepares our statement with connection info, all variables inside placeholders in sql:
+		$stmt = sqlsrv_prepare( $conn, $sql, array(&$userInput));
+		// checks the statement for errors, sqlsrv_prepare returns false if there's an error:
+		if( $stmt )  
+		{  
+			 echo '<script>console.log("Statement prepared.\n")</script>';  
+		}  
+		else  
+		{  
+			 echo '<script>console.log("Error in preparing statement.\n")</script>';  
+			 // $log = print_r( sqlsrv_errors(), true);  
+		} 
+		
+		// This actually uses the statement on the database, prints out errors if something happens:
+		if( sqlsrv_execute( $stmt))  
+		{  
+			  echo '<script>console.log("Statement executed.\n")</script>';  
+		}  
+		else  
+		{  
+			 echo '<script>console.log("Error in executing statement.\n")</script>';  
+			 // $log = print_r( sqlsrv_errors(), true);   
+		}  
+	
+	// now we need to load the results, generate a loop and have it echo off tables in the HTML here:
+	$row = sqlsrv_fetch_array($stmt);
+	// this is a frankenstein of different sources I still have to figure out between differences
+	// in using prepared statements vs normal queries
+	$result = $conn->query($sql);
+	if ($result->num_rows > 0) {
+	// output data of each row
+	while($row = $result->fetch_assoc()) {
+	echo "<tr><td>" . $row["id"]. "</td><td>" . $row["username"] . "</td><td>"
+	. $row["password"]. "</td></tr>";
+	}
+	echo "</table>";
+	} else { echo "0 results"; }
+	
+	// Closes the connection and also releases statement resources:
+	sqlsrv_free_stmt( $stmt);  
+	sqlsrv_close( $conn); 
+	}
+	// No need to use an else statement as default is do nothing
+}
+
+// This will be restricted to updates to the database
+if(isset($_POST['submit'])){
+	
+	function function_alert($message) { 
+      
+		// Display the alert box  
+		echo "<script>alert('$message');</script>"; 
+	}
+	
+	// Condition to check if there is no data given.
+	if ($_POST['product'] == "" or $_POST['update']=="" or $_POST['newUpdate']==""){
+		
+		// Function call 
+		function_alert("Missing Information. Please fulfill everything.");
+	
+	// Other conditions to update the database.	
+	}else{
+		// Will need to introduce a way to grab 
+		$product = $_POST['product'];
+		$selected2 = $_POST['update'];
+		
+		// Check wich part the user want to change, and use the SQL query to update it in the database.
+		
+		// TODO: implement prepared statement, load value from $_POST[update] into it
+		//------------------------------------------- 
+		// itemID is (PK, int, not null)
+		// productName is (varchar(60), null)
+		// productSKU is (varchar(10), null)
+		// itemDescription is (varchar(5000), null)
+		// price is (smallmoney, null)
+		//-------------------------------------------
+		// placeholders (?) are used in SQL statements to prepare a statement & prevent SQL injection
+		// apparently you cannot bind columns to parameters in prepared statements, so we have to introduce a function to sanitize the radio buttons
+		$sql = "UPDATE yellowteam.dbo.inventory SET ? VALUES (?)";
+				
+	}
+}
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+	<!-- documentation at http://getbootstrap.com/docs/4.1/, alternative themes at https://bootswatch.com/ -->
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
+
+	<link href="./css/styles.css" rel="stylesheet">
+	<link href="css/indexstyle.css" rel="stylesheet">
+	<title>Update Products</title>
+
+</head>
+
+<body>
+
+<div class="header">
+  <div class="links">
+  <a class="active" href="index.html">Home</a>
+  <a href="products.html">Products</a>
+  <a href="addinventory.php">Add Inventory</a>
+  <a href="updateinventory.php">Update Products</a>
+  <a href="contactus.html">Contact Us</a>
+  <a href="aboutus.html">FAQ</a>
+  </div>
+  <div class ="logo">
+  <img src="inverselogo.png" alt="Italian Trulli" style="width:4.5%;height:4.5%;">
+</div>
+</div>
+<br><br>
+
+<main class="container p-5">
+	<div><b> Update Products <b></div><br>
+	<form action="" method="POST">
+      <fieldset>
+       <p style="font-size:14px">Search Options:          
+          <input type = "radio"
+                 name = "searchOptions"
+                 value = "productSKU"
+				 id = "option1"
+                 checked = "checked" />
+          <label for = "option1">Product SKU (10 characters)</label>
+          <input type = "radio"
+                 name = "searchOptions"
+                 value = "itemDescription" 
+				 id = "option2" />
+          <label for = "option2">Item Description</label>
+          <input type = "radio"
+                 name = "searchOptions"
+                 id = "option3"
+                 value = "price" />
+          <label for = "option3">Price</label>
+		  <br><br>
+		  <input type="text" id="searchBox" name="searchInput" value=""><br>
+		  <br>
+		  <button name="searchPress" class="btn btn-dark">Search</button>
+        </p>       
+      </fieldset>     
+    </form>
+	<br></br>
+
+</main>
+
+	<footer>
+	<div>
+		Service provided by YellowTeam 2021
+	</div>
+	</footer>
+
+</body>
+
+</html>
