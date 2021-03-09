1d1edaeb792df08bfbf35a2cb9431a7027f829e2
Tomas Kasparaitis <61473801+t-kasparaitis@users.noreply.github.com>
2021-03-09 15:10:55 -0500

    Add files via upload
    - this was on the live server, but not on github

--- employees.php
+++ employees.php
@@ -1,66 +1,81 @@
 <?php
+
+// SQLSRV are Microsoft developed drivers, PDO_SQLSRV or PDO are community developed drivers for MSSQL
+// $serverName is just the URL our database is hosted on:
+$serverName = "database-1.cwszuet1aouw.us-east-1.rds.amazonaws.com";
+// $connection info is an array which can only take database, user & password:
+$connectionInfo = array( "Database"=>'yellowteam', "UID"=>'admin', "PWD"=>'$LUbx6*xTY957b6');
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+
 // checks if HTML element by the name of 'addInv' has been clicked
 // Note: PHP supports try catch blocks, error handling should be later rewritten using this logic
 if(isset($_POST['addemployee'])){
 	
-	// SQLSRV are Microsoft developed drivers, PDO_SQLSRV or PDO are community developed drivers for MSSQL
-	// $serverName is just the URL our database is hosted on:
-	$serverName = "database-1.cwszuet1aouw.us-east-1.rds.amazonaws.com";
-	// $connection info is an array which can only take database, user & password:
-	$connectionInfo = array( "Database"=>'yellowteam', "UID"=>'admin', "PWD"=>'$LUbx6*xTY957b6');
-	$conn = sqlsrv_connect( $serverName, $connectionInfo);
+	function function_alert($message) { 
+      
+		// Display the alert box  
+	echo "<script>alert('$message');</script>"; }
 	
-	// Connects to server, or spits out error log if it fails
-	if( $conn ) {
-		 echo '<script>console.log("Connection established.\n")</script>';
-	}else{
-		 echo '<script>console.log("Connection could not be established.\n");</script>';
-		 
-// placeholders (?) are used in SQL statements to prepare a statement & prevent SQL injection
-	$sql = "INSERT INTO yellowteam.dbo.employees (firstname, lastname, username, password)
-			VALUES (?, ?, ?, ?)";
-		
 	//----------------------------------------------------------------------------------------------------------------  
 	// Using $_POST['addInv'] to trigger form, and then using $_POST['name'] to pull user input into variables.
 	//----------------------------------------------------------------------------------------------------------------  
-	$firstname = $_POST['employeefirstname'];
-	$lastname = $_POST['employeelastname'];
-	$username = $_POST['username'];
-	$password = $_POST['password'];
 	
-	// prepares our statement with connection info, all variables inside placeholders in sql:
-	$stmt = sqlsrv_prepare( $conn, $sql, array(&$firstname, &$lastname, &$username, &$password));
+	if($_POST['password'] <> $_POST['repassword']){
+		
+		// Function call 
+		function_alert("The two passwords do not match. Please check.");
 	
-	// checks the statement for errors, sqlsrv_prepare returns false if there's an error:
-	if( $stmt )  
-	{  
-		 echo '<script>console.log("Statement prepared.\n")</script>';  
-	}  
-	else  
-	{  
-		 echo '<script>console.log("Error in preparing statement.\n")</script>';  
-		 // $log = print_r( sqlsrv_errors(), true);  
-	} 
-	
-	// This actually uses the statement on the database, prints out errors if something happens:
-	if( sqlsrv_execute( $stmt))  
-	{  
-		  echo '<script>console.log("Statement executed.\n")</script>';  
-	}  
-	else  
-	{  
-		 echo '<script>console.log("Error in executing statement.\n")</script>';  
-		 // $log = print_r( sqlsrv_errors(), true);   
-	}  
-	
-	// Closes the connection and also releases statement resources:
-	sqlsrv_free_stmt( $stmt);  
-	sqlsrv_close( $conn); 
+	// Other conditions to update the database.	
+	}else{
+		 
+		$password = $_POST['password'];
+		$firstname = $_POST['employeefirstname'];
+		$lastname = $_POST['employeelastname'];
+		$username = $_POST['username'];
+		
+	// placeholders (?) are used in SQL statements to prepare a statement & prevent SQL injection
+		$sql = "INSERT INTO yellowteam.dbo.employees (firstname, lastname, username, password)
+				VALUES (?, ?, ?, ?)";
+			
+		// prepares our statement with connection info, all variables inside placeholders in sql:
+		$stmt = sqlsrv_prepare( $conn, $sql, array(&$firstname, &$lastname, &$username, &$password));
+		
+		// checks the statement for errors, sqlsrv_prepare returns false if there's an error:
+		if( $stmt )  
+		{  
+			 echo '<script>console.log("Statement prepared.\n")</script>';  
+		}  
+		else  
+		{  
+			 echo '<script>console.log("Error in preparing statement.\n")</script>';  
+			 // $log = print_r( sqlsrv_errors(), true);  
+		} 
+		
+		// This actually uses the statement on the database, prints out errors if something happens:
+		if( sqlsrv_execute( $stmt))  
+		{  
+			  echo '<script>console.log("Statement executed.\n")</script>';  
+		}  
+		else  
+		{  
+			 echo '<script>console.log("Error in executing statement.\n")</script>';  
+			 // $log = print_r( sqlsrv_errors(), true);   
+		}  
+		
+		// Closes the connection and also releases statement resources:
+		sqlsrv_free_stmt( $stmt);  
+		sqlsrv_close( $conn); 
+		}
+	}
 
-}
 ?>
 <html>
 <head>
+
+ <!-- documentation at http://getbootstrap.com/docs/4.1/, alternative themes at https://bootswatch.com/ -->
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
+		
   <meta charset="utf-8">
   <title>Employees</title>
   <meta name="author" content="Team Yellow">
@@ -83,11 +98,13 @@ if(isset($_POST['addemployee'])){
 	  <a href="employees.php">Employees</a>
       </div>
 	  
-      <div class ="logo">
-      <img src="inverselogo.png" alt="Italian Trulli" style="width:4.5%;height:4.5%;">
-      </div>
+      
 </div>
 
+<br>
+
+
+<main class="container p-5">
 <div class="content">  
   <h1>Nuts and Bolts Hardware</h1>
   <h3>Employees Page</h3>
@@ -95,21 +112,34 @@ if(isset($_POST['addemployee'])){
   <!-- edited action="" since we don't have a PHP file to forward to, added method="POST" -Tomas -->
   <form action="" method="POST">
   <label for="employeefirstname">First Name:</label><br>
-  <input type="text" id="employeefirstname" name="employeefirstname" value=""><br>
+  <input type="text" id="employeefirstname" name="employeefirstname" placeholder="First Name" value=""><br>
   
   <label for="employeelastname">Last Name:</label><br>
-  <input type="text" id="employeelastname" name="employeelastname" value=""><br><br>
+  <input type="text" id="employeelastname" name="employeelastname" placeholder="Last Name" value=""><br>
   
     <label for="username">Username:</label><br>
-  <input type="text" id="username" name="username" value="" pattern="[a-z]"><br>
+  <input type="text" id="username" name="username" value="" placeholder="Username" pattern="[a-z].{1,}" title="username can only be lowercase with no spaces."><br>
   
     <label for="password">Password:</label><br>
-  <input type="text" id="password" name="password" value="" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*?[~`!@#$%\^&*()\-_=+[\]{};:\x27.,\x22\\|/?><])"><br>
+  <input type="text" id="password" name="password" value="" placeholder="Password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*?[~`!@#$%\^&*()\-_=+[\]{};:\x27.,\x22\\|/?><]).{1,}" title="Password must have at least one lowercase letter, one uppercase, one number, and a special character."><br>
+
+<label for="password">Re-enter Password:</label><br>
+  <input type="text" id="repassword" name="repassword" value="" placeholder="Re-enter Password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*?[~`!@#$%\^&*()\-_=+[\]{};:\x27.,\x22\\|/?><]).{1,}" title="Password must have at least one lowercase letter, one uppercase, one number, and a special character."><br>
+
   
-  <input type="submit" name="addemployee" value="Create Employee Account">
+	<br>
+  <button name="addemployee" value="Create Employee Account" class="btn btn-dark">Create Employee Account</button>
+
   
   </form>
 </div>
 
+       </main>
+
+        <footer>
+		<div>
+            Service provided by YellowTeam 2021
+		</div>
+        </footer>
 </body>
 </html>
