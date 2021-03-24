771c7abaa8b0f50ff0a395c76d3e9b75341ab088
Tomas Kasparaitis <61473801+t-kasparaitis@users.noreply.github.com>
2021-03-23 20:10:05 -0400

    images update
    - there's a no background logo & inverse logo
    - favicon has been added (little icon on browser tab)

--- index.php
+++ index.php
@@ -19,8 +19,13 @@ Tomas Kasparaitis
   <meta name="description" content="Nuts and bolts hardware company homepage">
   <meta name="keywords" content="Nuts and bolts, hardware, Nuts and bolts hardware, home, homepage">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-
   <link href="css/index.css" rel="stylesheet">
+
+  <!-- Generated using favicon.io, provides logo icon on browser tab; need these 4 lines to function: -->
+  <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+  <link rel="manifest" href="img/site.webmanifest">
 
 </head>
 
