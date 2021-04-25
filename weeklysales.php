988ce2324b75ea0273c9f62df3dd48f9273902bf
Yrassim <74272046+Yrassim@users.noreply.github.com>
2021-04-25 14:50:36 -0400

    Update weeklysales.php
    Removing .php extemsion from links.

--- weeklysales.php
+++ weeklysales.php
@@ -4,13 +4,18 @@ session_start();
 
 // Currently, Guests don't see their history, so redirect if not logged in:
 if(!isset($_SESSION["username"])) {
-    header("location: login.php");
+    header("location: login");
 }
 
 $serverName = "database-1.cwszuet1aouw.us-east-1.rds.amazonaws.com";
 // $connection info to the database
 $connectionInfo = array( "Database"=>'yellowteam', "UID"=>'admin', "PWD"=>'$LUbx6*xTY957b6');
 $conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+if(isset($_POST["genReport"])) {
+    $reportDate = strtotime($_POST["reportDate"]);
+    echo "<script>console.log($reportDate)</script>";
+}
 
 ?>
 
@@ -19,13 +24,17 @@ $conn = sqlsrv_connect( $serverName, $connectionInfo);
 
 <head>
     <meta charset="UTF-8">
-    <title>Weekly Sales</title>
+    <title>Products</title>
     <meta name="author" content="Team Yellow">
     <meta name="description" content="Nuts and bolts hardware company products page">
     <meta name="keywords" content="Nuts and bolts, hardware, Nuts and bolts hardware, products, inventory">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- documentation at http://getbootstrap.com/docs/4.1/, alternative themes at https://bootswatch.com/ -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet"> <!-- also makes page responsive -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <link href="css/main.css" rel="stylesheet">
     <!-- Generated using favicon.io, provides logo icon on browser tab; need these 4 lines to function: -->
     <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
@@ -37,62 +46,70 @@ $conn = sqlsrv_connect( $serverName, $connectionInfo);
 <body>
 <div class="header">
     <div class="links">
-        <a href="index.php">Home</a>
-        <a href="products.php">Products</a>
-        <a href="cart.php">Cart</a>
+        <a href="index">Home</a>
+        <a href="products">Products</a>
+        <a href="cart">Cart</a>
         <?php if(isset($_SESSION["accesslevel"])) {
             if ($_SESSION["accesslevel"] > 1) {
-                echo '<a href="addinventory.php">Add Inventory</a>';
+                echo '<a href="addinventory">Add Inventory</a>';
             }
         }?>
         <?php if(isset($_SESSION["accesslevel"])) {
             if ($_SESSION["accesslevel"] > 1) {
-                echo '<a href="updateinventory.php">Update Products</a>';
+                echo '<a href="updateinventory">Update Products</a>';
             }
         }?>
-        <a href="contactus.php">Contact Us</a>
-        <a href="aboutus.php">FAQ</a>
+        <a href="contactus">Contact Us</a>
+        <a href="aboutus">FAQ</a>
         <?php if(isset($_SESSION["accesslevel"])) {
             if ($_SESSION["accesslevel"] > 1) {
-                echo '<a href="employees.php">Employees</a>';
+                echo '<a href="employees">Employees</a>';
             }
         }?>
         <?php if(!isset($_SESSION["username"])) {
-            echo '<a href="login.php">Login</a>';
+            echo '<a href="login">Login</a>';
         }?>
         <?php if(isset($_SESSION["username"])) {
-            echo '<a href="history.php">Order History</a>';
+            echo '<a href="history">Order History</a>';
         }?>
         <?php if(isset($_SESSION["accesslevel"])) {
             if ($_SESSION["accesslevel"] > 1) {
-                echo '<a href="weeklysales.php">Weekly Sales</a>';
+                echo '<a href="weeklysales">Weekly Sales</a>';
             }
         }?>
         <?php if(isset($_SESSION["username"])) {
-            echo '<a href="logout.php">Logout</a>';
+            echo '<a href="logout">Logout</a>';
         }?>
     </div>
 </div>
 
 <main class="container p-5">
     <div><h3><b> Order History <b></h3></div><br>
+    <form action="" method="POST">
+        <label for="unsupportedBrowserDate">Select any day to generate a report for that week</label><br />
+        <input type="date" name="reportDate" id="unsupportedBrowserDate" class="inline" />
+        <button type="submit" name="genReport" class="btn btn-primary inline">Generate Report</button>
+    </form><br />
     <table class="table">
         <thead class="thead-dark">
         <tr>
-            <th scope="col">#</th>
-            <th scope="col">Date</th>
-            <th scope="col">Receipt #</th>
-            <th scope="col">Total</th>
+            <th scope="col">Item</th>
+            <th scope="col">Monday</th>
+            <th scope="col">Tuesday</th>
+            <th scope="col">Wednesday</th>
+            <th scope="col">Thursday</th>
+            <th scope="col">Friday</th>
+            <th scope="col">Saturday</th>
+            <th scope="col">Sunday</th>
         </tr>
         </thead>
 
         <tbody>
-        
-
-
         </tbody>
+
     </table>
 </main>
 
+<script>if ( $('#unsupportedBrowserDate')[0].type != 'date' ) $('#unsupportedBrowserDate').datepicker();</script>
 </body>
 </html>
