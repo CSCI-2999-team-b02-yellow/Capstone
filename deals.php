37b8a864a5d96ba80ed6ddea4a758985bcab842b
Yrassim <74272046+Yrassim@users.noreply.github.com>
2021-05-02 22:58:58 -0400

    Add files via upload
    Displays the great deals

---
+++ deals.php
@@ -0,0 +1,172 @@
+<?php
+
+session_start();
+
+$serverName = "database-1.cwszuet1aouw.us-east-1.rds.amazonaws.com";
+// $connection info to the database
+$connectionInfo = array( "Database"=>'yellowteam', "UID"=>'admin', "PWD"=>'$LUbx6*xTY957b6');
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+   <style>
+   
+   .invoice-box {
+  max-width: 800px;
+  margin: auto;
+  padding: 30px;
+  border: 1px solid #eee;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
+  font-size: 16px;
+  line-height: 24px;
+  font-family: "Helvetica Neue", "Helvetica", Helvetica, Arial, sans-serif;
+  color: #555;
+}
+
+.invoice-box table {
+  width: 100%;
+  line-height: inherit;
+  text-align: left;
+}
+
+.invoice-box table td {
+  padding: 5px;
+  vertical-align: top;
+}
+
+.invoice-box table tr td:nth-child(n + 2) {
+  text-align: right;
+}
+
+.invoice-box table tr.top table td {
+  padding-bottom: 20px;
+}
+
+.invoice-box table tr.top table td.title {
+  font-size: 45px;
+  line-height: 45px;
+  color: #333;
+}
+
+.invoice-box table tr.information table td {
+  padding-bottom: 40px;
+}
+
+.invoice-box table tr.heading td {
+  background: #eee;
+  border-bottom: 1px solid #ddd;
+  font-weight: bold;
+}
+
+.invoice-box table tr.details td {
+  padding-bottom: 20px;
+}
+
+.invoice-box table tr.item td {
+  border-bottom: 1px solid #eee;
+}
+
+.invoice-box table tr.item.last td {
+  border-bottom: none;
+}
+
+.invoice-box table tr.item input {
+  padding-left: 5px;
+}
+
+.invoice-box table tr.item td:first-child input {
+  margin-left: -5px;
+  width: 100%;
+}
+
+.invoice-box table tr.total td:nth-child(2) {
+  border-top: 2px solid #eee;
+  font-weight: bold;
+}
+
+.invoice-box input[type="number"] {
+  width: 60px;
+}
+
+@media only screen and (max-width: 600px) {
+  .invoice-box table tr.top table td {
+    width: 100%;
+    display: block;
+    text-align: center;
+  }
+
+  .invoice-box table tr.information table td {
+    width: 100%;
+    display: block;
+    text-align: center;
+  }
+}
+
+/** RTL **/
+.rtl {
+  direction: rtl;
+  font-family: Tahoma, "Helvetica Neue", "Helvetica", Helvetica, Arial,
+    sans-serif;
+}
+
+.rtl table {
+  text-align: right;
+}
+
+.rtl table tr td:nth-child(2) {
+  text-align: left;
+}
+
+   
+   </style>
+  </head>
+  <body>
+<div class="invoice-box">
+  <table cellpadding="0" cellspacing="0">
+    <tr class="top">
+      <td colspan="4">
+        <table>
+          <tr>
+            <td class="title">
+				Nuts and Bolts Great Deal
+            </td>
+          
+          </tr>
+        </table>
+      </td>
+    </tr>
+
+    <tr class="heading">
+	 
+      <td>Product</td>
+
+      <td>Price</td>
+	  <td></td>
+    </tr>
+	
+    <tr class="item" v-for="item in items">
+	
+	<?php
+	$sql= "SELECT * FROM yellowteam.dbo.inventory WHERE deal = 1";
+	$stmt = sqlsrv_prepare($conn, $sql);
+	sqlsrv_execute($stmt);
+	$count = 1;
+    while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
+    ?>
+	
+      <td><img src=".\images\<?php echo ($row["itemID"]); ?>.jpg" alt="Image Test" style="width:50px;height:60px;"> <?php echo $row["productName"]; ?></td>
+
+      <td><?php echo '$'.number_format($row["price"], 2, '.', ','); ?></td>
+	  
+    </tr>
+    
+	<?php }
+	?>
+  </table>
+
+</div>
+  </body>
+</html>
